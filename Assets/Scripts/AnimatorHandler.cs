5ed2efb2469232893aa8a4efcdf7cbbf04849172
Petr Hampl <2007hampl@seznam.cz>
2022-04-17 22:49:46 +0200

    Added: Character movement and imported packages with assets and animations

---
+++ Assets/Scripts/AnimatorHandler.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class AnimatorHandler : MonoBehaviour
+    {
+        public Animator animator;
+        private int vertical;
+        private int horizontal;
+        public bool canRotate;
+
+        public void Initialize()
+        {
+            animator = GetComponent<Animator>();
+            vertical = Animator.StringToHash("Vertical");
+            horizontal = Animator.StringToHash("Horizontal");
+        }
+
+        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
+        {
+            #region Vertical
+            float v = 0f;
+            if (verticalMovement > 0 && verticalMovement < 0.55f)
+            {
+                v = 0.5f;
+            }
+            else if (verticalMovement > 0.55f)
+            {
+                v = 1f;
+            }
+            else if (verticalMovement < 0 && verticalMovement > -0.55f)
+            {
+                v = -0.5f;
+            }
+            else if (verticalMovement < -0.55f)
+            {
+                v = -1f;
+            }
+            else
+            {
+                v = 0f;
+            }
+            #endregion
+            #region Horizontal
+            float h = 0f;
+            if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+            {
+                h = 0.5f;
+            }
+            else if (horizontalMovement > 0.55f)
+            {
+                h = 1f;
+            }
+            else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+            {
+                h = -0.5f;
+            }
+            else if (horizontalMovement < -0.55f)
+            {
+                h = -1f;
+            }
+            else
+            {
+                h = 0f;
+            }
+            #endregion
+            
+            animator.SetFloat(vertical, v, 0.1f,Time.deltaTime);
+            animator.SetFloat(horizontal, h, 0.1f,Time.deltaTime);
+        }
+
+        public void CanRotate()
+        {
+            canRotate = true;
+        }
+
+        public void StopRotation()
+        {
+            canRotate = false;
+        }
+    }
+}
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public float horizontal;
+    [SerializeField] public float vertical;
+    [SerializeField] public float moveAmount;
+    [SerializeField] private float mouseX;
+    [SerializeField] private float mouseY;
+
+    private PlayerControls inputActions;
+
+    private Vector2 movementInput;
+    private Vector2 cameraInput;
+
+    public void OnEnable()
     {
-        
+        if (inputActions == null)
+        {
+            inputActions = new PlayerControls();
+            inputActions.PlayerMovement.Movement.performed += inputActions
+                => movementInput = inputActions.ReadValue<Vector2>();
+            inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+        }
+        inputActions.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDisable()
     {
-        
+        inputActions.Disable();
     }
+
+    public void TickInput(float delta)
+    {
+        MoveInput(delta);
+    }
+
+    private void MoveInput(float delta)
+    {
+        horizontal = movementInput.x;
+        vertical = movementInput.y;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+        mouseX = cameraInput.x;
+        mouseY = cameraInput.y;
+
+    }
+    
+    
+    
+    
+    
 }
