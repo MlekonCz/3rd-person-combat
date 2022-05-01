a6554bf35d14d863e36c488bc919f76fdf982f0f
Petr Hampl <2007hampl@seznam.cz>
2022-05-01 20:47:27 +0200

    Added: Weapons base script

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private WeaponSlotManager _weaponSlotManager;
+ 
+ 
+    public WeaponItem rightWeapon;
+    public WeaponItem leftWeapon;
+
+    private void Awake() 
+    { 
+        _weaponSlotManager = GetComponentInChildren<WeaponSlotManager>();
+        
+    }
+
+    private void Start()
+    {
+        _weaponSlotManager.LoadWeaponOnSlot(rightWeapon, false);
+        _weaponSlotManager.LoadWeaponOnSlot(leftWeapon,true);
+    }
+}
---
+++ Assets/Scripts/WeaponItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Item/Weapon", order = 0)]
+public class WeaponItem : Item
+{ 
+    public GameObject modelPrefab;
+    public bool isUnarmed;
+
+
+
+}
